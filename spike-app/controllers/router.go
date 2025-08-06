5f2e90cd5d4d5b024f59c7b1c60f6af3c7d3276d
kubotake-desktop <keigo1003560@gmail.com>
2025-08-06 22:57:52 +0900

    Refactor template path handling in router to support different environments

--- spike-app/controllers/router.go
+++ spike-app/controllers/router.go
@@ -1,12 +1,24 @@
 package controllers
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
+// テスト時: tests/からtemplates/を探すため../templates/*が必要
+// 本番時: spike-app/からtemplates/を探すためtemplates/*が必要
+func getTemplatePath() string {
+	if _, err := os.Stat("templates"); err == nil {
+		return "templates/*"
+	}
+	return "../templates/*"
+}
+
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
+
+	r.LoadHTMLGlob(getTemplatePath())
 	r.GET("/hello", Hello)
 
 	r.GET("/recipes", ListRecipe)
