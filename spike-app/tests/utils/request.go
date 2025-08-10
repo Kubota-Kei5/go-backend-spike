106b3dce38452945e011d5be7d7ac2ffb8c74d8a
kubotake-desktop <keigo1003560@gmail.com>
2025-08-10 17:38:16 +0900

    Refactor RouterRequest to accept request body for enhanced testing capabilities

--- spike-app/tests/utils/request.go
+++ spike-app/tests/utils/request.go
@@ -3,12 +3,21 @@ package testutil
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func RouterRequest(r *gin.Engine, method, path string) *httptest.ResponseRecorder {
-	req, _ := http.NewRequest(method, path, nil)
+func RouterRequest(r *gin.Engine, method, path string, body string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, strings.NewReader(body))
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func RouterJSONRequest(r *gin.Engine, method, path string, body string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	return w
