62fa40615c5883d710c0c4a4da935377bf4a9836
kubotake-desktop <keigo1003560@gmail.com>
2025-08-11 09:38:37 +0900

    Improve recipe retrieval and creation logic

--- spike-app/controllers/recipe_controller.go
+++ spike-app/controllers/recipe_controller.go
@@ -16,7 +16,12 @@ func NewRecipe(c *gin.Context) {
 }
 
 func ListRecipe(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{})
+	recipes, err := models.GetAllRecipes()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+	c.HTML(http.StatusOK, "index.html", gin.H{"recipes": recipes})
 }
 
 func TestCreateRecipe(c *gin.Context) {
@@ -32,6 +37,41 @@ func TestCreateRecipe(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, testCreatedRecipe)
+}
+
+type FormRecipeInfo struct {
+	Title                string  `form:"title" binding:"required"`
+	Servings             int     `form:"servings" binding:"required"`
+	CookingTime          int     `form:"cooking_time" binding:"required"`
+	IngredientNames      []string `form:"ingredient_names" binding:"required"`
+	IngredientQuantities []int    `form:"ingredient_quantities" binding:"required"`
+}
+
+func CreateRecipe(c *gin.Context) {
+	var form FormRecipeInfo
+	var recipe models.Recipe
+	// var ingredient models.Ingredients
+	// var recipeIngredient models.RecipeIngredient
+
+	if err := c.ShouldBindJSON(&form); err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	recipe.Title = form.Title
+	recipe.Servings = form.Servings
+	recipe.CookingTime = form.CookingTime
+
+
+	// recipeIngredient.Quantities = make([]int, 0, len(form.IngredientQuantities))
+	// recipe.Ingredients = make([]models.RecipeIngredient, 0, len(form.IngredientNames))
+
+	createdRecipe, err := recipe.Create()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+	c.JSON(http.StatusOK, createdRecipe)
 }
 
 func GetRecipe(c *gin.Context) {
